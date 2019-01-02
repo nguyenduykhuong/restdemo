4e58a8a6de3fe90ae4326d6be5398cdbcb848b12
nguyenduykhuong <nguyenduykhuongco102@gmail.com>
2019-01-02 14:51:53 +0700

    demo2

---
+++ example_html_css/js/app.js
@@ -0,0 +1,9 @@
+ 
+var App = angular.module('myApp',[]);
+ 
+ App.controller('Ctrcontroller', ['$scope', function($scope){
+ 	$scope.x='nguyenduy';
+ 	console.log(myForm.inputid.$valid);
+ 	console.log("z");
+ 	console.log($scope.x);
+ }])
